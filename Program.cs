9e4a7782ff5d7f195a7dc8b896d01a7882b1a205
DineshEedhuru <dinesheedhuru123@gmail.com>
2025-02-17 15:09:25 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,10 +12,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World");
             Console.WriteLine("Hai this is the C# Programming");
-
-            Console.WriteLine("C# is a programming language and Dot net is a framework " +
-                "and it is used to develop web Applications");
-
+ 
              
         }
     }
