77a3474fbe0ec6d7e1c209d413f931ac08b35fc3
DineshEedhuru <dinesheedhuru123@gmail.com>
2025-02-17 15:08:26 +0530

    removed a new line

--- Program.cs
+++ Program.cs
@@ -16,8 +16,7 @@ namespace ConsoleApp1
             Console.WriteLine("C# is a programming language and Dot net is a framework " +
                 "and it is used to develop web Applications");
 
-
-            Console.WriteLine("This is added a new Line by me");
+             
         }
     }
 }
